9bee6ede17bbfdc98e9dc9611821fa607f715d48
Arianna Garassino <g.arianna26@gmail.com>
2018-03-14 10:01:15 +0100

    creato header e contenitore con bootstrap

--- demoBootAngular/src/app/app.module.ts
+++ demoBootAngular/src/app/app.module.ts
@@ -1,18 +1,25 @@
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
